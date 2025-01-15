1455259437ee6c2c9e433243360c82a8580f226b
Aakash <aakashmohole@gmail.com>
2025-01-15 19:11:03 +0530

    15-1 2DArrayProblems

---
+++ Array2DProblems/rotateMatrix90Degrees48.java
@@ -0,0 +1,49 @@
+package Array2DProblems;
+
+public class rotateMatrix90Degrees48 {
+    // 1 convert the row to columns
+    // 2 revers the matrix row wise
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        rotate(matrix);
+    }
+    public static void rotate(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i+1; j < matrix.length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        System.out.println("Matrix after conversion: ");
+        printMatrix(matrix);
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            int start = 0;
+            int end = matrix.length - 1;
+            while (start < end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+        System.out.println();
+        System.out.println("Final ans: ");
+        printMatrix(matrix);
+    }
+
+    public static void printMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+        }
+    }
+}
