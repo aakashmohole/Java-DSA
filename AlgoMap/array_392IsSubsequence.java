5254e142ae87f551f0c7b428c413f67a6f363a01
Aakash <aakashmohole@gmail.com>
2025-01-21 21:36:16 +0530

    21-01 problems done

--- AlgoMap/array_392IsSubsequence.java
+++ AlgoMap/array_392IsSubsequence.java
@@ -2,19 +2,22 @@ package AlgoMap;
 
 public class array_392IsSubsequence {
     public static void main(String[] args) {
-       String s = "abc", t = "ahbgdc";
+       String s = "acb", t = "ahbgdc";
         System.out.println(isSubsequence(s, t));
     }
 
     public static boolean isSubsequence(String s, String t) {
-
+        //s = "acb", t = "ahbgdc"
+        int j = 0;
         String dup = "";
         for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < t.length(); j++) {
+            while (j < t.length()){
                 if (s.charAt(i) == t.charAt(j)){
                     dup = dup + t.charAt(j);
+                    j++;
                     break;
                 }
+                j++;
             }
         }
         if (dup.equals(s)){
