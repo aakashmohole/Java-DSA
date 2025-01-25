253d4cf67b5461ca9bad2d76e72b517de76410e4
Aakash <aakashmohole@gmail.com>
2025-01-25 18:47:44 +0530

    Hashing

---
+++ HashMapProblems/MajorityElementII229.java
@@ -0,0 +1,23 @@
+package HashMapProblems;
+
+import java.util.*;
+
+public class MajorityElementII229 {
+    public static void main(String[] args) {
+        int[] arr= {3,2,3};
+        System.out.println(majorityElement(arr));
+    }
+    public static List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
+        }
+        List<Integer> res = new ArrayList<>();
+        for (Integer key : hm.keySet()){
+            if (hm.get(key) > nums.length/3){
+                res.add(key);
+            }
+        }
+        return res;
+    }
+}
---
+++ HashMapProblems/majorityElements169.java
@@ -0,0 +1,33 @@
+package HashMapProblems;
+import java.util.*;
+public class majorityElements169 {
+    public static void main(String[] args){
+        int[] arr = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(arr));
+    }
+
+    public static int majorityElement(int[] nums) {
+        //create hashmap
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+//            if (hm.containsKey(nums[i])){
+//                hm.put(nums[i], hm.get(nums[i]) + 1);
+//            }else {
+//                hm.put(nums[i], 1);
+//            }
+
+            //optimal
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
+        }
+
+        //get all keys
+        for(Integer key : hm.keySet()){
+            if (hm.get(key) > nums.length/3){
+                return key;
+            }
+        }
+        return -1;
+    }
+}
