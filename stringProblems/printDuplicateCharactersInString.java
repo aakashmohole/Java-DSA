de98ec52d4302078b7f2c9ab29a1fbb0253616cf
Aakash <aakashmohole@gmail.com>
2025-01-26 20:03:38 +0530

    26-01 problems

--- stringProblems/printDuplicateCharactersInString.java
+++ stringProblems/printDuplicateCharactersInString.java
@@ -1,24 +1,26 @@
 package stringProblems;
 
+import java.util.*;
+
+
 public class printDuplicateCharactersInString {
     public static void main(String[] args) {
         String s = "geeksforgeeks";
         printDuplicateCharacters(s);
     }
     public static void printDuplicateCharacters(String s) {
-        // Create an array to count character frequencies (for 'a' to 'z')
-        int[] count = new int[256]; // ASCII range
+        HashMap<Character, Integer> map =  new HashMap<>();
 
-        // Count occurrences of each character
         for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i)]++;
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
         }
 
-        // Print characters with count greater than 1
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] > 1) {
-                System.out.println((char) i + ", count = " + count[i]);
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
             }
         }
+
+
     }
 }
--- stringProblems/removeConsecutiveCharacters.java
+++ stringProblems/removeConsecutiveCharacters.java
@@ -8,7 +8,6 @@ public class removeConsecutiveCharacters {
     public static String remove(String s){
         StringBuilder sb =  new StringBuilder("");
 
-//        sb.append( s.charAt(0));
         for (int i = 0; i < s.length(); i++) {
             if (i < s.length() - 1  && s.charAt(i) == s.charAt(i+1)){
                 i++;
