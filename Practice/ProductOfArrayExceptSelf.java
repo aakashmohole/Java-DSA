2c19a95f10145472f6db825cd4a532e1adcb5465
Aakash <aakashmohole@gmail.com>
2025-02-20 22:41:53 +0530

    revision problem

--- Practice/ProductOfArrayExceptSelf.java
+++ Practice/ProductOfArrayExceptSelf.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class ProductOfArrayExceptSelf {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4};
-        System.out.println(Arrays.toString(productExceptSelfPrefixSuffix(arr)));
+        System.out.println(Arrays.toString(productExceptSelfPrefixSuffixOptimal(arr)));
 //        System.out.println(find(arr));
     }
     static ArrayList<Integer> find(int[] arr){
@@ -52,7 +52,21 @@ public class ProductOfArrayExceptSelf {
         return ans;
     }
 
+    public static int[] productExceptSelfPrefixSuffixOptimal(int[] nums){
 
+        int[] ans =  new int[nums.length];
+        ans[0] =1;
+        //prefix
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        int suffix = 1;
+        for (int i = nums.length-2; i >=0; i--) {
+            suffix *= nums[i+1];
+            ans[i] *= suffix;
+        }
 
+        return ans;
+    }
 
 }
