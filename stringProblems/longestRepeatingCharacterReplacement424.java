5a4cd27a8780d7f85e17e3aa4a8ec54ff47f7f4b
Aakash <aakashmohole@gmail.com>
2025-03-07 17:24:02 +0530

    neetcode Problem

--- stringProblems/longestRepeatingCharacterReplacement424.java
+++ stringProblems/longestRepeatingCharacterReplacement424.java
@@ -2,24 +2,24 @@ package stringProblems;
 
 public class longestRepeatingCharacterReplacement424 {
     public static void main(String[] args) {
-        String  s = "AABABBA"; int k = 1;
-        System.out.println(returnCharacterReplacement(s, k));
+        String  s = "AABABBA"; int k = 2;
+        System.out.println(characterReplacement(s, k));
     }
-    public static int returnCharacterReplacement(String s, int k){
-        int start =0, i = 0;
-        String ans = "";
-        while (i < s.length()){
+    public static int characterReplacement(String s, int k) {
+        int[] occurance = new int[26];
+        int left = 0;
+        int ans = 0;
+        int maxOccurance = 0;
 
-            if (s.charAt(start) == s.charAt(i)){
-                ans = ans+s.charAt(i);
-                i++;
-            }else {
-                ans = ans+s.charAt(start);
-                k = k-1;
-                i++;
+        for(int right=0; right< s.length(); right++){
+            maxOccurance = Math.max(maxOccurance, ++occurance[s.charAt(right) - 'A']);
+            if(right - left+1 - maxOccurance > k){
+
+                occurance[s.charAt(left) - 'A']--;
+                left++;
             }
-
+            ans = Math.max(ans, right-left+1);
         }
-        return ans.length();
+        return ans;
     }
 }
